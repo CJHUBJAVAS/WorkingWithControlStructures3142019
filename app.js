78a4366fb5acdaffe6d93de4d9b5aa2a8224e837
CPCC Classwork <cjohn177@email.cpcc.edu>
2019-03-18 15:26:00 -0400

    working on it 3/18/19 315pm

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-first line but no code
+//returns a random integer in the range [m, n] (inclusive)
+function rand(m, n) {
+    return m + Math.floor((n - m +1)*Math.random());
+}
+//randomly returns a string representing one of the six
+//Crown and Anchor faces
+function randFace() {
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"]
+        [rand(0, 5)];
+}
+
+let funds = 50;      //starting conditions
+let round = 0;
+
+while(funds > 0 && funds < 100) {
+    round++;
+    console.log(`round ${round}:`);
+    console.log(`\tstarting funds: ${funds}p`);
+    
+    //place bets
+
+    //roll dice
+
+    //collect winnings
+
+}
